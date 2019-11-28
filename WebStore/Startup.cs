07b04104e45a819b9dd3a8682ecbdc7bd7568d38
helloworld <pickletacocat@gmail.com>
2019-11-28 13:56:42 +0300

    Add project files.

---
+++ WebStore/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuraton { get;  }
+        public Startup(IConfiguration Config) { Configuraton = Config; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
