acd696855d55a96ccaccfccf692c7fee5125c8e6
AmasingCoconut <58276535+AmasingCoconut@users.noreply.github.com>
2019-11-28 17:07:34 +0300

    The first controller

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -8,10 +8,13 @@ namespace WebStore
 {
     public class Startup
     {
+
+        // основная инфраструктура приложения - 3 раздела: контроллеры, представления и модели
         public IConfiguration Configuration { get;  }
         public Startup(IConfiguration Config) { Configuration = Config; }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +25,21 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes => 
             {
-                await context.Response.WriteAsync(Configuration["CustomData"]);
+                routes.MapRoute(
+                    name: "default", 
+                    "{controller=Home}/{action=Index}/{id?}");
             });
+            //app.UseMvcWithDefaultRoute(); // тоже самое, что написано вверху: произойдет такое же конфигурирование системы маршрутизации приложения
+            //mvc терминальное по, ниже нельзя добавлять обработчики конвеера. 
+            //Если добавить обработчик выше (app.run()), то конвеер не дойдет до системы mvc, потому что ысе запросы будут обрабатываться выше.
+            // для системы mvc необходимо сконфигурировать маршруты
+
+            //app.run(async (context) =>
+            //{
+            //    await context.response.writeasync(configuration["customdata"]);
+            //});
         }
     }
 }
